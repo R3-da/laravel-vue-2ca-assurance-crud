e2a0ad10c0631e30054f17c1bea899e84ff3c4ad
r3-da <haddan.reda.net@gmail.com>
2025-02-11 01:59:01 +0100

    feat(Login): redirect user to claims page after sign-in

--- resources/js/src/pages/Login.vue
+++ resources/js/src/pages/Login.vue
@@ -44,7 +44,7 @@ const onSignIn = async () => {
 
     if (user?.id) {
         userStore.setUser(user);
-        await pushToRoute({ name: 'users' });
+        await pushToRoute({ name: 'claims' });
     }
 };
 </script>
